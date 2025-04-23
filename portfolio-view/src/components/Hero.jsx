a88b2ae850dcc2174968d956c2241804a6fbc66b
akshay-sanap <akshaynsanap@gmail.com>
2025-04-23 17:35:49 +0530

    Buttons ongoing

--- portfolio-view/src/components/Hero.jsx
+++ portfolio-view/src/components/Hero.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+
+
+
+
 const Hero = () => {
   return (
     <section id="home" className="pt-28 lg:pt-36">
@@ -17,17 +21,21 @@ const Hero = () => {
             </figure>
             <div className="flex items-center gap-1.5 text-sm tracking-wide text-zinc-400">
               <span className="relative h-2 w-2 rounded-full bg-emerald-400">
-                <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
+                <span className="absolute inset-0 animate-ping rounded-full bg-emerald-400"></span>
               </span>
               Available for Work
             </div>
           </div>
-          <h2 className="headline-1">Bulding Scalable Websites For the Future</h2>
-          <div className="">ButtonPrimary ButtonOutline</div>
+          <h2 className="headline-1 mb-8 mt-5 max-w-[15ch] sm:max-w-[20ch] lg:mb-10 lg:max-w-[15ch]">
+            Bulding Scalable Websites For the Future
+          </h2>
+          <div className="flex items-center gap-3">
+            ButtonPrimary ButtonOutline
+          </div>
         </div>
 
-        <div className="">
-          <figure className="">
+        <div className="hidden lg:block">
+          <figure className="ml-auto w-full max-w-[480px] overflow-hidden rounded-[60px] bg-gradient-to-t from-sky-400 via-sky-400/40 via-25% to-65%">
             <img
               src="/images/hero-banner.png"
               width={656}
--- portfolio-view/src/components/Navbar.jsx
+++ portfolio-view/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 
 const Navbar = ({ navOpen }) => {
   const lastActiveLink = useRef();
@@ -7,57 +7,57 @@ const Navbar = ({ navOpen }) => {
 
   const initActiveBox = () => {
     console.log(lastActiveLink.current);
-    activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
-    activeBox.current.style.left = lastActiveLink.current.offLeft + "px";
-    activeBox.current.style.width = lastActiveLink.current.offWidth + "px";
-    activeBox.current.style.height = lastActiveLink.current.offHeight + "px";
+    activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
+    activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
+    activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
+    activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px';
   };
 
   useEffect(initActiveBox, []);
-  window.addEventListener("resize", initActiveBox);
+  window.addEventListener('resize', initActiveBox);
 
   const activeCurrentLink = (e) => {
-    lastActiveLink.current?.classList.remove("active");
-    e.target.classList.add("active");
+    lastActiveLink.current?.classList.remove('active');
+    e.target.classList.add('active');
     lastActiveLink.current = e.target;
 
-    activeBox.current.style.top = e.target.offsetTop + "px";
-    activeBox.current.style.left = e.target.offsetLeft + "px";
-    activeBox.current.style.width = e.target.offsetWidth + "px";
-    activeBox.current.style.height = e.target.offsetHeight + "px";
+    activeBox.current.style.top = e.target.offsetTop + 'px';
+    activeBox.current.style.left = e.target.offsetLeft + 'px';
+    activeBox.current.style.width = e.target.offsetWidth + 'px';
+    activeBox.current.style.height = e.target.offsetHeight + 'px';
   };
 
   const navItems = [
     {
-      label: "Home",
-      link: "#home",
-      className: "nav-link active",
+      label: 'Home',
+      link: '#home',
+      className: 'nav-link active',
       ref: lastActiveLink,
     },
     {
-      label: "About",
-      link: "#about",
-      className: "nav-link",
+      label: 'About',
+      link: '#about',
+      className: 'nav-link',
     },
     {
-      label: "Work",
-      link: "#work",
-      className: "nav-link",
+      label: 'Work',
+      link: '#work',
+      className: 'nav-link',
     },
     {
-      label: "Reviews",
-      link: "#reviews",
-      className: "nav-link",
+      label: 'Reviews',
+      link: '#reviews',
+      className: 'nav-link',
     },
     {
-      label: "Contact",
-      link: "#contact",
-      className: "nav-link md:hidden",
+      label: 'Contact',
+      link: '#contact',
+      className: 'nav-link md:hidden',
     },
   ];
 
   return (
-    <nav className={"navbar" + (navOpen ? " active" : "")}>
+    <nav className={'navbar' + (navOpen ? ' active' : '')}>
       {navItems.map(({ label, link, className, ref }, key) => (
         <a
           href={link}
